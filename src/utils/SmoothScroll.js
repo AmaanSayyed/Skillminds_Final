c2d9f807b7ee93be1461e175eff0627aae1a533e
Amaan Sayyed <sayamaan55@gmail.com>
2024-08-01 21:31:22 +0530

    updated

--- src/utils/SmoothScroll.js
+++ src/utils/SmoothScroll.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 const SmoothScroll = () => {
     useEffect(() => {
         const script = document.createElement('script');
-        script.src = './assets/js/smooth-scroll.js'; // Ensure the correct path to smoothscroll.js
+        script.src = '/assets/js/smooth-scroll.js'; // Ensure the correct path to smoothscroll.js
         script.async = true;
     
         document.body.appendChild(script);
